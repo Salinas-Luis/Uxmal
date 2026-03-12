1902c43116a91fbb1d422ab60cd353a695a8c5c3
Salinas-Luis <lsalinasr2300@alumno.ipn.mx>
2026-03-11 19:54:13 -0600

    actualizacion de ip3

--- back/app.js
+++ back/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json()); 
 app.use(session({
     secret: 'keyboard cat', 
-    resave: false,
+    resave: true,
     saveUninitialized: false,
     cookie: { 
         secure: true, 
